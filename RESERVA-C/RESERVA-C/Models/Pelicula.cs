ff3db66a451ee221948298889ada4e487c28e10c
GianmarcoZodda <zoddagj@gmail.com>
2023-08-30 22:25:47 -0300

    Propiedades  para clases Pelicula y Persona

--- RESERVA-C/RESERVA-C/Models/Pelicula.cs
+++ RESERVA-C/RESERVA-C/Models/Pelicula.cs
@@ -2,5 +2,17 @@
 {
     public class Pelicula
     {
+
+        public DateTime FechaLanzamiento { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Genero Genero { get; }
+
+        public List<Funcion> Funciones { get; set; }
+
+
     }
 }
--- RESERVA-C/RESERVA-C/Models/Persona.cs
+++ RESERVA-C/RESERVA-C/Models/Persona.cs
@@ -2,5 +2,13 @@
 {
     public class Persona
     {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaAlta { get; set; }
+
     }
 }
