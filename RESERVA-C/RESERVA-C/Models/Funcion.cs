1f52c95646cdd08660cf35b98729d71cd11db49d
CarlosPergamo <carlospergamo@gmail.com>
2023-08-30 22:24:43 -0300

    Propiedades de la clase Reserva y Funcion

--- RESERVA-C/RESERVA-C/Models/Funcion.cs
+++ RESERVA-C/RESERVA-C/Models/Funcion.cs
@@ -2,5 +2,24 @@
 
 public class Funcion
 {
+    public int Id { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public DateTime Hora { get; set; }
+
+    public string Descripcion { get; set; }
+
+    public int ButacasDisponibles { get; set; }
+
+    public bool Confirmada { get; set; }
+
+    public Pelicula Pelicula { get; set; }
+
+    public Sala Sala { get; set; }
+
+    public List<Reserva> Reservas { get; set; }
+
+
 
 }
--- RESERVA-C/RESERVA-C/Models/Reserva.cs
+++ RESERVA-C/RESERVA-C/Models/Reserva.cs
@@ -2,5 +2,15 @@
 
 public class Reserva
 {
+    public  int Id { get; set; }
+
+    public Funcion Funcion { get; set; }
+
+    public DateTime FechaAlta { get; set; }
+
+    public Cliente Cliente { get; set; }
+
+    public int CantidadButacas { get; set; }
+
 
 }
