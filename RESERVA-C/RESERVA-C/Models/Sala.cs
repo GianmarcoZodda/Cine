e58e1d7e6364de95fcef627dde969997c7f92940
CarlosPergamo <carlospergamo@gmail.com>
2023-08-30 22:48:49 -0300

    Cambio en Sala

--- RESERVA-C/RESERVA-C/Models/Sala.cs
+++ RESERVA-C/RESERVA-C/Models/Sala.cs
@@ -3,4 +3,14 @@
 public class Sala
 {
 
+    public int Id { get; set; }
+
+    public int Numero { get; set; }
+    
+    public string TipoSala { get; set; }
+    
+    public int CapacidadButacas { get; set; }
+    
+    public List<Funcion> Funciones { get; set; }
+
 }
