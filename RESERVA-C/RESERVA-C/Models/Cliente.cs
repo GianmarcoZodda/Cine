43675657f51d019d9e93dd7afa1de69f582a4898
IgnacioKoch <nacho_francos@hotmail.com>
2023-09-04 14:07:06 -0300

    Actualizacion de branch 'ignacio'. Actualizacion de Cliente & Empleado que tenian redundancia de datos de la clase padre Persona.

--- RESERVA-C/RESERVA-C/Models/Cliente.cs
+++ RESERVA-C/RESERVA-C/Models/Cliente.cs
@@ -2,14 +2,12 @@
 {
     public class Cliente : Persona
     {
-        public int ID { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public int DNI { get; set; }
         public string Telefono { get; set; }
         public string Direccion { get; set; }
         public DateTime FechaAlta { get; set; }
-        public string Email { get; set; }
         public List<Reserva> Reservas { get; set; }
 
 
--- RESERVA-C/RESERVA-C/Models/TipoSala.cs
+++ RESERVA-C/RESERVA-C/Models/TipoSala.cs
@@ -2,7 +2,7 @@
 {
     public class TipoSala
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Nombre { get; set; }
         public int Precio { get; set; }
 
