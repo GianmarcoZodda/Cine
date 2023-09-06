133ec2015d66d22d636800de45a0ab666de0ca9f
Ignacio Francos Koch <nacho_francos@hotmail.com>
2023-09-06 19:13:04 -0300

    Hotfix en atributo 'ID', se cambio a 'Id'

--- RESERVA-C/RESERVA-C/Models/TipoSala.cs
+++ RESERVA-C/RESERVA-C/Models/TipoSala.cs
@@ -2,7 +2,7 @@
 {
     public class TipoSala
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public int Precio { get; set; }
 
