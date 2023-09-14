dbb7635a2b89508eabbc4a3426063ae6946d5cde
Nahuezedavid <nahueldavid150101@gmail.com>
2023-09-14 15:35:27 -0300

    restricciones de Funcion y Genero

--- RESERVA-C/RESERVA-C/Models/Funcion.cs
+++ RESERVA-C/RESERVA-C/Models/Funcion.cs
@@ -1,17 +1,40 @@
-﻿namespace RESERVA_C.Models
+﻿using RESERVA_C.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace RESERVA_C.Models
 {
     public class Funcion
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [DataType(DataType.Date)]
         public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [DataType(DataType.Time)]
         public DateTime Hora { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(200, MinimumLength = 15, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Descripcion { get; set; }
+
+        //Esto va a ser una propiedad computada teniendo en cuenta la sala de la funcion y las reservas de esta
         public int ButacasDisponibles { get; }
+
         public bool Confirmada { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
         public int PeliculaId { get; set; }
+
         public Pelicula Pelicula { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
         public int SalaId { get; set; }
+
         public Sala Sala { get; set; }
+
         public List<Reserva> Reservas { get; set; }
 
     }
--- RESERVA-C/RESERVA-C/Models/Genero.cs
+++ RESERVA-C/RESERVA-C/Models/Genero.cs
@@ -1,9 +1,17 @@
-﻿namespace RESERVA_C.Models
+﻿using RESERVA_C.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace RESERVA_C.Models
 {
     public class Genero
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Nombre { get; set; }
+
         public List<Pelicula> Peliculas { get; set; }
     }
 }
