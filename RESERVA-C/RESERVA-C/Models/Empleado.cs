b7b859ab24276c5e3410f5785cd0b918fdb765b4
IgnacioKoch <nacho_francos@hotmail.com>
2023-09-15 16:05:54 -0300

    Restricciones TipoSala, Empleado & Cliente.

--- RESERVA-C/RESERVA-C/Models/Empleado.cs
+++ RESERVA-C/RESERVA-C/Models/Empleado.cs
@@ -1,12 +1,32 @@
-﻿namespace RESERVA_C.Models
+﻿using System.ComponentModel.DataAnnotations;
+using RESERVA_C.Helpers;
+
+namespace RESERVA_C.Models
 {
     public class Empleado : Persona
     {
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Apellido { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Range(10000000, 99999999, ErrorMessage = ErrorMsgs.MaxMin)]
         public int DNI { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Range(100000000, 999999999, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Telefono { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Direccion { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Legajo { get; set; }
 
     }
--- RESERVA-C/RESERVA-C/Models/TipoSala.cs
+++ RESERVA-C/RESERVA-C/Models/TipoSala.cs
@@ -1,9 +1,17 @@
-﻿namespace RESERVA_C.Models
+﻿using RESERVA_C.Helpers;
+using System.ComponentModel.DataAnnotations;
+namespace RESERVA_C.Models
 {
     public class TipoSala
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required (ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = ErrorMsgs.MaxMin)]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Range(1, int.MaxValue, ErrorMessage = ErrorMsgs.MaxMin)]
         public int Precio { get; set; }
 
     }
