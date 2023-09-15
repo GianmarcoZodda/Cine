f924599f1a72d561816873df66e7fb00dc119f70
CarlosPergamo <carlospergamo@gmail.com>
2023-09-15 18:37:41 -0300

    Restricciones Reserva & Sala

--- RESERVA-C/RESERVA-C/Models/Reserva.cs
+++ RESERVA-C/RESERVA-C/Models/Reserva.cs
@@ -1,15 +1,22 @@
-﻿namespace RESERVA_C.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESERVA_C.Models
 { 
 public class Reserva
 {
+
+    [Key]
     public  int Id { get; set; }
 
     public Funcion Funcion { get; set; }
 
+    [Required(ErrorMessage = ErrorMsgs.Required)]
+    [DataType(DataType.Date)]
     public DateTime FechaAlta { get; set; }
 
     public Cliente Cliente { get; set; }
 
+    [Required(ErrorMessage = ErrorMsgs.Required)]
     public int CantidadButacas { get; set; }
 
 
--- RESERVA-C/RESERVA-C/Models/Sala.cs
+++ RESERVA-C/RESERVA-C/Models/Sala.cs
@@ -1,14 +1,18 @@
-﻿namespace RESERVA_C.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESERVA_C.Models
 {
     public class Sala
     {
-
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = ErrorMsgs.Required)]
         public int Numero { get; set; }
 
         public TipoSala TipoSala { get; set; }
 
+        [Required(ErrorMessage = ErrorMsgs.Required)]
         public int CapacidadButacas { get; set; }
 
         public List<Funcion> Funciones { get; set; }
