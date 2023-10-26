d8c4ebd9e12a8f44d6675a2bf9ee03d4e14594e4
Nahuezedavid <nahueldavid150101@gmail.com>
2023-10-26 17:41:43 -0300

    display de peliculaId y salaID en Funcion

--- RESERVA-C/RESERVA-C/Models/Funcion.cs
+++ RESERVA-C/RESERVA-C/Models/Funcion.cs
@@ -33,11 +33,13 @@ namespace RESERVA_C.Models
         public bool Confirmada { get; set; }
 
         [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Display(Name = "Pelicula")]
         public int PeliculaId { get; set; }
 
         public Pelicula Pelicula { get; set; }
 
         [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Display(Name = "Sala")]
         public int SalaId { get; set; }
 
         public Sala Sala { get; set; }
