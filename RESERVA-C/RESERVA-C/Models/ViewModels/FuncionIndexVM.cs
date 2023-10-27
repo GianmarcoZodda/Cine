c875867814d21b51404e2fdbd9987ac9a441f3f6
Nahuezedavid <nahueldavid150101@gmail.com>
2023-10-27 11:08:31 -0300

    Creacion del view model del Index de Funciones para el calculo de butacas

---
+++ RESERVA-C/RESERVA-C/Models/ViewModels/FuncionIndexVM.cs
@@ -0,0 +1,54 @@
+﻿using RESERVA_C.Helpers;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace RESERVA_C.Models.ViewModels
+{
+    public class FuncionIndexVM
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Display(Name = "Fecha y Hora")]
+        public DateTime FechaHora { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }//propiedad autoimplementada para que el get sea de la prop FechaHora
+
+        [NotMapped]
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [DataType(DataType.Time)]
+        public DateTime Hora { get; set; }//propiedad autoimplementada para que el get sea de la prop FechaHora
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [StringLength(200, MinimumLength = 15, ErrorMessage = ErrorMsgs.MaxMin)]
+        public string Descripcion { get; set; }
+
+        //Esto va a ser una propiedad computada teniendo en cuenta la sala de la funcion y las reservas de esta
+        [Display(Name = "Butacas Disponibles")]
+        public int ButacasDisponibles { get; set; }
+
+        public bool Confirmada { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Display(Name = "Pelicula")]
+        public int PeliculaId { get; set; }
+
+        public Pelicula Pelicula { get; set; }
+
+        [Required(ErrorMessage = ErrorMsgs.Required)]
+        [Display(Name = "Sala")]
+        public int SalaId { get; set; }
+
+        public Sala Sala { get; set; }
+
+        public List<Reserva> Reservas { get; set; }
+
+        public string FuncionCompleta
+        { get { return $"{Pelicula.Titulo} - {FechaHora} - Sala: {Sala.Numero}"; } }
+    }
+}
